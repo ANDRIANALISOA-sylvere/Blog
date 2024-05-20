82ac2306fd5574bdca9844d9ccb61c0989cbc320
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2024-05-21 00:12:38 +0300

    new categorie

---
+++ app/Http/Controllers/Api/CategorieController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Categorie;
+use Illuminate\Http\Request;
+
+class CategorieController extends Controller
+{
+    function show()
+    {
+        $categorie = Categorie::all();
+
+        return response()->json($categorie, 200);
+    }
+
+    function index(Request $request)
+    {
+        $categorie = new Categorie();
+
+        $categorie->name = $request->name;
+        $categorie->slug = $request->slug;
+
+        $categorie->save();
+
+        return response()->json($categorie, 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CategorieController;
 use App\Http\Controllers\API\CommentController;
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
@@ -21,6 +22,9 @@ Route::post("post", [PostController::class, 'index']);
 
 Route::get("comment", [CommentController::class, 'show']);
 Route::post("comment", [CommentController::class, 'index']);
+
+Route::get("categorie",[CategorieController::class,'show']);
+Route::post("categorie",[CategorieController::class,'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();

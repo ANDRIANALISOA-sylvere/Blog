931e4c6726334385718cf17d89c0191c37ba200d
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2024-05-20 21:35:21 +0300

    new post

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    function show()
+    {
+        $post = Post::all();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $post
+        ]);
+    }
+
+    function index(Request $request)
+    {
+        $post = new Post();
+
+        $post->title = $request->title;
+        $post->slug = $request->slug;
+        $post->content = $request->content;
+        $post->featured_image = $request->featured_image;
+        $post->status = $request->status;
+        $post->published_at = $request->published_at;
+        $post->user_id = $request->user_id;
+
+        $post->save();
+
+        return response()->json([
+            'status' => 201,
+            'data' => $post
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get("post", [PostController::class, 'show']);
+Route::post("post", [PostController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();

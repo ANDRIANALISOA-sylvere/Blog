22c4d700898a7568fe53baa6ca96dc71c2ec49a7
josephin sylvère <josephinsylvere@gmail.com>
2024-08-16 00:36:58 +0300

    update the model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
